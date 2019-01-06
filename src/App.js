7b27f65e6328ee44795520683bdb7bd8ebd6083c
Subahibi <subahibi@tuta.io>
2019-01-05 23:33:36 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header">
+          <h2>Live Cryptocurrency Prices</h2>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
